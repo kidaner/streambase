e487db7baa2ba03caea0aaaf5520f6b8a0f2fb78
Robel Kidane <kidaner@gmail.com>
2023-02-28 03:41:25 -0500

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import streamlit as st
 
+# Set the maximum column width to 50 characters
+pd.set_option('display.max_colwidth', 50)
 
 st.set_page_config(page_title="playbook",
                    page_icon=":notebook:",
@@ -9,6 +11,13 @@ st.set_page_config(page_title="playbook",
 df = pd.read_excel(io="latest.xlsx",
                    engine="openpyxl",
                    sheet_name="latest")
+
+# Format the date column to display in the format "2-23"
+df["Date"] = pd.to_datetime(df["Date"]).dt.strftime("%-m-%d")
+
+# Set the maximum column width of the "Text" column to 100 characters
+df.style.set_properties(
+    subset=["Text"], **{'max-width': '100px', 'text-wrap': 'break-word'})
 
 st.dataframe(df)
 
