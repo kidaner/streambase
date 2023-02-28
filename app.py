b85167a02819978a8bd20533864631995b18e29e
Robel Kidane <kidaner@gmail.com>
2023-02-28 04:23:34 -0500

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import streamlit as st
-import pyfiglet
 
 # Set the maximum column width to 50 characters
 pd.set_option('display.max_colwidth', 50)
@@ -9,8 +8,6 @@ pd.set_option('display.max_colwidth', 50)
 st.set_page_config(page_title="playbook",
                    page_icon=":notebook:",
                    layout="wide")
-
-st.title("playbook")
 
 df = pd.read_excel(io="latest.xlsx",
                    engine="openpyxl",
@@ -34,14 +31,3 @@ label = st.sidebar.multiselect("Select label:", options=df["Label"].unique(),
 df_selection = df.query("Category in @category & Label in @label")
 
 st.dataframe(df_selection)
-
-ascii_art = pyfiglet.figlet_format("scargo")
-
-# Display the ASCII art using st.code
-st.code(ascii_art, language="text")
-
-  ____
- /',__\
-/\__, `\
-\/\____/
- \/___/
